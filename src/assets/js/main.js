1a8b0e00831488f72e64db391dc0e8865795e214
Phil Wareham <phil@designhive.com>
2013-12-01 08:58:49 +0000

    Remove responsive-nav options that are default values

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -81,13 +81,7 @@
     require(['responsivenav'], function ()
     {
         responsiveNav('.nav-collapse', {
-            animate: true,       // Boolean: Use CSS3 transitions, true or false
-            transition: 400,     // Integer: Speed of the transition, in milliseconds
-            label: 'Menu',       // String: Label for the navigation toggle
-            insert: 'after',     // String: Insert the toggle before or after the navigation
-            customToggle: '',    // Selector: Specify the ID of a custom toggle
-            openPos: 'relative', // String: Position of the opened nav, relative or static
-            jsClass: 'js'        // String: 'JS enabled' class which is added to <html> el
+            transition: 400
         });
     });
 
