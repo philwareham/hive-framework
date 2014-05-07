746054daf1b2b94b3ee23cb27c50d610f98d70a4
Phil Wareham <phil@designhive.com>
2014-05-07 11:40:55 +0100

    Update jQuery to latest versions

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,10 +6,10 @@
 
     // Detect whether jQuery v2 features required, otherwise use jQuery v1 for higher compatibility.
 
-    var jqueryVersion = '1.11.0';
+    var jqueryVersion = '1.11.1';
 
     if (typeof JSON !== 'undefined' && 'querySelector' in document && 'addEventListener' in window) {
-        jqueryVersion = '2.1.0';
+        jqueryVersion = '2.1.1';
     }
 
     requirejs.config({
