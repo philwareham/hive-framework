51910bfbb0ed25fa96bc677eb71e1e2cfaca3aa8
Phil Wareham <phil@designhive.com>
2014-10-06 15:49:02 +0100

    Tweaks to jQuery UI init

--- public/mockups/jquery-ui-testing.js
+++ public/mockups/jquery-ui-testing.js
@@ -11,20 +11,32 @@ require(['main'], function ()
          * jQuery UI interactions.
          */
 
-        // Draggable
+        /**
+         * Draggable
+         */
+
         $('.jquery-ui-draggable').draggable({
             cursor: 'move'
         });
 
-        // Resizable
+        /**
+         * Resizable.
+         */
+
         $('.jquery-ui-resizable').resizable({
             minWidth: 200
         });
 
-        // Selectable
+        /**
+         * Selectable.
+         */
+
         $('.jquery-ui-selectable').selectable();
 
-        // Sortable
+        /**
+         * Sortable.
+         */
+
         $('.jquery-ui-sortable').sortable({
             placeholder: 'ui-sortable-placeholder',
             cursor: 'ns-resize'
@@ -34,24 +46,39 @@ require(['main'], function ()
          * jQuery UI widgets.
          */
 
-        // Accordion
+        /**
+         * Accordion.
+         */
+
         $('.jquery-ui-accordion').accordion({
             header: 'h3'
         });
 
-        // Autocomplete
+        /**
+         * Autocomplete.
+         */
+
         var countryList = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo, Democratic Republic', 'Congo, Republic of the', 'Costa Rica', 'Cote d’Ivoire', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Greenland', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Mongolia', 'Morocco', 'Monaco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Samoa', 'San Marino', ' Sao Tome', 'Saudi Arabia', 'Senegal', 'Serbia and Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'];
         $('#autocomplete-country').autocomplete({
             source: countryList
         });
 
-        // Button
+        /**
+         * Button.
+         */
+
         $('.jquery-ui-button').button();
 
-        // Button set
+        /**
+         * Button set.
+         */
+
         $('.jquery-ui-buttonset').buttonset();
 
-        // Icon button
+        /**
+         * Icon button.
+         */
+
         $('.jquery-ui-button-icon-only').button({
             icons: {
                 primary: 'ui-icon-locked'
@@ -82,7 +109,10 @@ require(['main'], function ()
             text: false
         });
 
-        // Split button
+        /**
+         * Split button.
+         */
+
         $('#split-rerun-example')
             .button()
             .click(function ()
@@ -115,7 +145,10 @@ require(['main'], function ()
             .hide()
             .menu();
 
-        // Split button search example
+        /**
+         * Split button search example.
+         */
+
         $('.txp-search-button')
             .button({
                 text: false,
@@ -141,7 +174,7 @@ require(['main'], function ()
                     at: 'left bottom',
                     of: this
                 });
-                $(document).one('click', function ()
+                $(document).one('click blur', function ()
                 {
                     menu.hide();
                 });
@@ -153,7 +186,10 @@ require(['main'], function ()
             .hide()
             .menu();
 
-        // Options button
+        /**
+         * Options button.
+         */
+
         $('#detail-toggle').button({
             text: false,
             icons: {
@@ -161,22 +197,34 @@ require(['main'], function ()
             }
         });
 
-        // Datepicker
+        /**
+         * Datepicker.
+         */
+
         $('.jquery-ui-datepicker').datepicker().children().show();
 
-        // Dialog (basic)
+        /**
+         * Dialog (basic).
+         */
+
         $('#jquery-ui-dialog').dialog({
             autoOpen: false
         });
 
-        // Dialog (modal) link
+        /**
+         * Dialog (modal) link.
+         */
+
         $('#jquery-ui-dialog-link').click(function ()
         {
             $('#jquery-ui-dialog').dialog('open');
             return false;
         });
 
-        // Dialog (modal)
+        /**
+         * Dialog (modal).
+         */
+
         $('#jquery-ui-dialog-modal').dialog({
             autoOpen: false,
             modal: true,
@@ -193,22 +241,38 @@ require(['main'], function ()
             }
         });
 
-        // Dialog (modal) link
+        /**
+         * Dialog (modal) link.
+         */
+
         $('#jquery-ui-dialog-modal-link').click(function ()
         {
             $('#jquery-ui-dialog-modal').dialog('open');
             return false;
         });
 
-        // Menu
-        $('.jquery-ui-menu').menu();
+        /**
+         * Menu.
+         */
 
-        // Progressbar
+        $('.jquery-ui-menu').menu({
+            position: {
+                at: 'right top-1'
+            }
+        });
+
+        /**
+         * Progressbar.
+         */
+
         $('.jquery-ui-progressbar').progressbar({
             value: 37
         });
 
-        // Progressbar (indeterminate)
+        /**
+         * Progressbar (indeterminate).
+         */
+
         $('.jquery-ui-progressbar2').progressbar({
             value: false
         });
@@ -222,16 +286,25 @@ require(['main'], function ()
             event.preventDefault();
         });
 
-        // Selectmenu
+        /**
+         * Selectmenu.
+         */
+
         $('.jquery-ui-selectmenu').selectmenu();
 
-        // Slider - horizontal
+        /**
+         * Slider - horizontal.
+         */
+
         $('.jquery-ui-slider-horizontal').slider({
             range: true,
             values: [17, 67]
         });
 
-        // Slider - vertical
+        /**
+         * Slider - vertical.
+         */
+
         $('.jquery-ui-slider-vertical > span').each(function ()
         {
             var value = parseInt($(this).text());
@@ -243,7 +316,10 @@ require(['main'], function ()
             });
         });
 
-        // Spinner
+        /**
+         * Spinner.
+         */
+
         $('#jquery-ui-spinner').spinner({
             spin: function (event, ui)
             {
@@ -260,10 +336,16 @@ require(['main'], function ()
             }
         });
 
-        // Tabs
+        /**
+         * Tabs.
+         */
+
         $('.jquery-ui-tabs').tabs();
 
-        // Tooltips
+        /**
+         * Tooltips.
+         */
+
         $('.jquery-ui-tooltip').tooltip();
 
     });
