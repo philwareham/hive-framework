0b07fad10a77c837bddf58970276768941cc9c8d
Phil Wareham <phil@designhive.com>
2016-01-11 10:24:35 +0000

    Update jQuery

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,10 +6,10 @@
 
     // Detect whether jQuery v2 features required, otherwise use jQuery v1 for higher compatibility.
 
-    var jqueryVersion = '1.11.3';
+    var jqueryVersion = '1.12.0';
 
     if (typeof JSON !== 'undefined' && 'querySelector' in document && 'addEventListener' in window) {
-        jqueryVersion = '2.1.4';
+        jqueryVersion = '2.2.0';
     }
 
     requirejs.config({
