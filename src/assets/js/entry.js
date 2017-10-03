5f59de98e8846124f0bd086d2b8224e8c11ae66d
Phil Wareham <phil@designhive.com>
2017-10-03 16:20:46 +0100

    Testiong some Webpack

---
+++ src/assets/js/entry.js
@@ -0,0 +1,31 @@
+// Testing some Webpack stuff.
+
+
+// Import NPM modules.
+
+// Syntax highlighting, via 'Prism'.
+// Applies syntax highlighting to `code` HTML elements.
+// More info - http://prismjs.com.
+
+import autosize from 'autosize'
+
+// Syntax highlighting, via 'Prism'.
+// Applies syntax highlighting to `code` HTML elements.
+// More info - http://prismjs.com.
+
+import Prism from 'prismjs/components/prism-core'
+
+// Google Analytics, via 'ga-lite'.
+// More info - https://github.com/jehna/ga-lite.
+
+import galite from 'ga-lite'
+galite('create', 'UA-XXXXXXXX-X', 'auto')
+galite('send', 'pageview')
+
+
+// DOM ready code goes in here
+ready(() => {
+    autosize({
+        autosize(fields);
+    });
+});
