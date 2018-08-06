1787508577ef62d0e039d9784f5890a3ca415014
Phil Wareham <phil@designhive.com>
2018-08-06 11:23:48 +0100

    Faster builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function (grunt)
 {
     'use strict';
 
+    // Define Sass implmentation (Dart-Sass).
+    const Fiber = require('fibers');
     const sass = require('dart-sass');
 
     // Load all Grunt tasks.
@@ -163,6 +165,7 @@ module.exports = function (grunt)
         // Sass configuration.
         sass: {
             options: {
+                fiber: Fiber,
                 implementation: sass,
                 outputStyle: 'expanded', // outputStyle = expanded, nested, compact or compressed.
                 sourceMap: false
