bd7130f5b6ae5b4407382a75a2970fcca34c32a2
Phil Wareham <phil@designhive.com>
2019-01-21 15:50:40 +0000

    Add Media Quary packer

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,6 +130,7 @@ module.exports = function (grunt)
             options: {
                 processors: [
                     require('autoprefixer'),
+                    require("css-mqpacker"),
                     require('cssnano')
                 ]
             },
