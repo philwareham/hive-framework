e7c066d439f5396e03d5b1203ae4eb976cf4c3e4
Phil Wareham <phil@designhive.com>
2019-03-22 10:29:32 +0000

    Dark Mode helpers
    TODO: change this from jQuery to vanilla JavaScript

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -67,3 +67,73 @@ import Glide from '@glidejs/glide';
     }
 
 })();
+
+// Specific dark and light images.
+// Example:
+// <img src="assets/images/feature.png"
+//     data-src-light="assets/images/example.png"
+//     data-src-dark="assets/images/example-dark.png"
+//     srcset="assets/images/example@2x.png 2x"
+//     data-srcset-light="assets/images/example@2x.png 2x"
+//     data-srcset-dark="assets/images/example-dark@2x.png 2x" />
+
+function makeImagesDark()
+{
+    'use strict';
+
+    $('img.color-specific').each(function() {
+        $(this).attr('src', $(this).attr('data-dark-src'));
+        $(this).attr('srcset', $(this).attr('data-dark-srcset'));
+    });
+}
+
+function makeImagesLight()
+{
+    'use strict';
+
+    $('img.color-specific').each(function() {
+        $(this).attr('src', $(this).attr('data-light-src'));
+        $(this).attr('srcset', $(this).attr('data-light-srcset'));
+    });
+}
+
+// Detect Dark Mode/Light Mode.
+
+function toggleDarkMode(isDark)
+{
+    'use strict';
+
+    if (isDark.matches) {
+        makeImagesDark();
+    } else {
+        makeImagesLight();
+    }
+}
+
+var isDark = window.matchMedia('screen and (prefers-color-scheme: dark)');
+toggleDarkMode(isDark);
+isDark.addListener(toggleDarkMode);
+
+// Switch between Dark Mode/Light Mode manually.
+
+$('#lightswitch').on('click', function()
+{
+    'use strict';
+
+    if ($('body').hasClass('darkmode')) {
+        $('body').removeClass('darkmode');
+        makeImagesLight();
+        localStorage.removeItem("prefers-color-scheme", "dark");
+        return false;
+    } else {
+        $('body').addClass('darkmode');
+        makeImagesDark();
+        localStorage.setItem("prefers-color-scheme", "dark");
+        return false;
+    }
+});
+
+if (localStorage.getItem("prefers-color-scheme") === "dark") {
+    $('body').addClass('darkmode');
+    makeImagesDark();
+}
