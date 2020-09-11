b17082408e49f44b8b40b02ad9dc2d997fa89c9e
philwareham <phil@designhive.com>
2020-09-11 15:37:58 +0100

    Start work on modernising header/nav area

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -164,5 +164,50 @@ import Glide from '@glidejs/glide';
 
         e.preventDefault();
     });
-
 })();
+
+// Hide/show header on scroll
+// By Osvaldas Valutis www.osvaldas.info
+
+(function(document, window) {
+	'use strict';
+
+	var elSelector = '.header',
+		element = document.querySelector(elSelector);
+
+	if (!element) {
+        return true;
+    }
+
+	var elHeight		= 0,
+		elTop			= 0,
+		dHeight			= 0,
+		wHeight			= 0,
+		wScrollCurrent	= 0,
+		wScrollBefore	= 0,
+		wScrollDiff		= 0;
+
+	window.addEventListener( 'scroll', function() {
+		elHeight		= element.offsetHeight;
+		dHeight			= document.body.offsetHeight;
+		wHeight			= window.innerHeight;
+		wScrollCurrent	= window.pageYOffset;
+		wScrollDiff		= wScrollBefore - wScrollCurrent;
+		elTop			= parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+
+		if( wScrollCurrent <= 0 ) {
+			element.style.top = '0px';
+        } else if (wScrollDiff > 0) {
+			element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
+        } else if (wScrollDiff < 0) {
+			if (wScrollCurrent + wHeight >= dHeight - elHeight) {
+				element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
+            } else {
+				element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px';
+            }
+		}
+
+		wScrollBefore = wScrollCurrent;
+	});
+
+}(document, window, 0));
